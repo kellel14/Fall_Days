30827cb01ba4be5e1efbce23a0c2b35d91320309
kellel14 <kellel14@gmail.com>
2019-06-20 16:09:34 -0400

    initial push: layout

---
+++ src/Fall_Days.java
@@ -0,0 +1,60 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Fall_Days {
+//this is where im thinking about using different methods then calling that
+//method into the loop when by in main class as it radomizes the answer
+//public static int DaysofWeek(int a){
+//    Random random1 = new Random();
+//    int num1 = random1.nextInt(a);
+//    return num1;
+//}
+//
+//public static int drinkType(int b){
+//    Random random2 = new Random();
+//    int num2 = random2.nextInt();
+//    return num2;
+//    ArrayList<String>
+//}
+
+    //scanner here
+
+    public static void main (String[] args){
+        ArrayList<String> DayofWeek = new ArrayList<>();
+        DayofWeek.add("Sunday");
+        DayofWeek.add("Monday");
+        DayofWeek.add("Tuesday");
+        DayofWeek.add("Wednesday");
+        DayofWeek.add("Thursday");
+        DayofWeek.add("Friday");
+        DayofWeek.add("Saturday");
+        DayofWeek.add("Sunday");
+        DayofWeek.add("Sunday");
+
+        System.out.println("What's the Day?");
+        userInput.nextLine();
+
+        ArrayList<String> drinkType = new ArrayList<>();
+        drinkType.add("pumpkin spiced latte");
+        drinkType.add("chai tea");
+        drinkType.add("hot cocoa");
+
+        ArrayList<String> leafColor = new ArrayList<>();
+        leafColor.add("red");
+        leafColor.add("brown");
+        leafColor.add("yellow");
+        leafColor.add("orange");
+
+        //for loop method, first question
+
+
+ //continues to next question then repeats until user stops -> look back at 8 ball project
+//the process between user and prompt
+//if user response is
+        while(true){
+
+        }
+
+    }
+}
